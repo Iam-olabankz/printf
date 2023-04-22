6230d14c105e3e4a6bef13b6d8e7eaabfe2ece8d
Benson Oluwasemilogo <bensonn5151@gmail.com>
2023-04-22 07:13:42 -0700

    task

---
+++ printf.c
@@ -0,0 +1,74 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdarg.h>
+/**
+ * _printf - prints output according to a format
+ * @format: the format string
+ *
+ * Return: the number of characters printed
+ */
+
+int print_s(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		_putchar(s[i]);
+		i++;
+	}
+
+	return (i);
+}
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+int _printf(const char *format, ...)
+{
+
+	va_list args;
+	int i = 0, count = 0;
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				i++;
+				if (format[i] == 'c')
+				{
+					count += _putchar(va_arg(args, int));
+				}
+				else if (format[i] == 's')
+				{
+					count += print_s(va_arg(args, char *));
+				}
+				else if (format[i] == '%')
+				{
+					count += _putchar('%');
+				}
+				else
+				{
+					_putchar('%');
+					_putchar(format[i]);
+					count += 2;
+				}
+			}
+			else
+			{
+				_putchar(format[i]);
+				count++;
+			}
+			i++;
+		}
+		va_end(args);
+
+		return (count);
+	}
+
