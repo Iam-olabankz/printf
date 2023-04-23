7a1a7ef8836eedd48205e356351df3a7908b273c
Iam-olabankz <samsonolayemi200@gmail.com>
2023-04-23 06:45:54 -0700

    updated

--- get_precision.c
+++ get_precision.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * get_precision - Calculates the precision for printing.
  *
- * Return: Precision.
+ * @format: Formatted string in which to print the arguments.
+ * @i: Index of the '%' character.
+ * @list: List of arguments to be printed.
+ *
+ * Return: The precision value.
  */
 int get_precision(const char *format, int *i, va_list list)
 {
@@ -14,14 +15,17 @@ int get_precision(const char *format, int *i, va_list list)
 	int precision = -1;
 
 	if (format[curr_i] != '.')
-		return precision;
+		return (precision);
 
 	precision = 0;
 
-	for (curr_i++; format[curr_i] != '\0'; curr_i++)
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
 		if (is_digit(format[curr_i]))
-			precision = precision * 10 + format[curr_i] - '0';
+		{
+			precision *= 10;
+			precision += format[curr_i] - '0';
+		}
 		else if (format[curr_i] == '*')
 		{
 			curr_i++;
@@ -34,6 +38,6 @@ int get_precision(const char *format, int *i, va_list list)
 
 	*i = curr_i - 1;
 
-	return precision;
+	return (precision);
 }
 
