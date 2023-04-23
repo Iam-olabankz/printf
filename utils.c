f845319b287b12a5a981948312c0b73d32524cd2
Iam-olabankz <samsonolayemi200@gmail.com>
2023-04-23 06:38:55 -0700

    updated

--- utils.c
+++ utils.c
@@ -15,16 +15,17 @@ int is_printable(char c)
 }
 
 /**
- * append_hexa_code - Append ascci in hexadecimal code to buffer
- * @buffer: Array of chars.
+ * append_hexa_code - Append ASCII in hexadecimal code to buffer
+ * @ascii_code: ASCII code to be converted to hexadecimal and appended.
+ * @buffer: Array of chars to which hexadecimal code will be appended.
  * @i: Index at which to start appending.
- * @ascii_code: ASSCI CODE.
- * Return: Always 3
+ *
+ * Return: Always 3.
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
-	/* The hexa format code is always 2 digits long */
+	/* The hexadecimal format code is always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
@@ -54,7 +55,7 @@ int is_digit(char c)
 /**
  * convert_size_number - Casts a number to the specified size
  * @num: Number to be casted.
- * @size: Number indicating the type to be casted.
+ * @size: Number indicating the type to which num will be casted.
  *
  * Return: Casted value of num
  */
@@ -71,7 +72,7 @@ long int convert_size_number(long int num, int size)
 /**
  * convert_size_unsgnd - Casts a number to the specified size
  * @num: Number to be casted
- * @size: Number indicating the type to be casted
+ * @size: Number indicating the type to which num will be casted
  *
  * Return: Casted value of num
  */
